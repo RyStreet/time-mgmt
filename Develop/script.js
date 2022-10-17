c7103bb00e22f8f71d3a993aede0ac1605306b60
Ryan Street <ryanstreet122@gmail.com>
2022-10-17 15:43:30 -0400

    time class change function added

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,50 @@
 //Display current time on page header//
 
-var currentDay = document.getElementById("currentDay"); //gets empty <p> tag //
+const currentDay = document.getElementById("currentDay"); //gets empty <p> tag //
 
 var now = document.createTextNode(dayjs().format("dddd, MMMM DD")); //creates text node that includes dayjs format for current date//
 
 currentDay.appendChild(now) //appends currentDay text to empty <p> tag//
 
 
-// var button = document.getElementsByClassName("save")[0];
-
-// button.addEventListener("click", saveTask);
 
 
-// function saveTask(event) {
-//     event.preventDefault();
-//     console.log("save")
-//     // var task = document.getElementById("userText").value;
-//     var task = $(this).siblings("#userText").val();
-//     localStorage.setItem("task", task);
-// }
+// adds CSS class based on time //
+function addClass(){
+
+console.log("add Class")
+console.log(dayjs().hour())
+
+// currentTime set to current number of hours
+var currentTime = dayjs().hour();
+console.log(currentTime)
+
+$(".time-blocks").each(function ( ){
+    
+    var hourBlock = parseInt($(this).attr('id').split('hour')[1]);
+    console.log(hourBlock)
+
+    if(hourBlock < currentTime){
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+    }
+
+    else if(hourBlock === currentTime){
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+    }
+
+    else{
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    }
+});
+}
+
+
 
 $(".btn").click(function (e) {
     e.preventDefault();
@@ -27,3 +53,4 @@ $(".btn").click(function (e) {
     localStorage.setItem("task", task);
 });
 
+addClass();
